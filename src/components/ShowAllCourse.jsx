35e9945f7a34aa073894bac853719e4ec63243da
S Raviteja <rteja6935@gmail.com>
2025-07-17 01:28:32 +0530

    made changes

--- src/components/ShowAllCourse.jsx
+++ src/components/ShowAllCourse.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import '../CSS/ShowAllCourse.css'; 
+import '../CSS/Showallcourse.css'; 
 function ShowAllCourse() {
   const [offerings, setOfferings] = useState([]);
   const [registeredIds, setRegisteredIds] = useState([]);
